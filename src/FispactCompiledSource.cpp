77554fbb2cf960a712f3007bd9d092eb5c613666
Bill Ellis <billellis.07@gmail.com>
2026-02-19 19:56:14 +0000

    Updated sampling routines for 15.4

--- src/FispactCompiledSource.cpp
+++ src/FispactCompiledSource.cpp
@@ -236,12 +236,13 @@ openmc::SourceSite FizzyCompiledSource::sample(uint64_t *seed) const {
   // Sample a position within the volume of our chosen chosen element
   particle.r = sampleElementVolume(seed, mesh_id_, element_id);
   // Sample an isotropic angle
-  particle.u = angle_->sample(seed);
-
+  particle.u = angle_->sample(seed).first;
   // Sample isotropic angle
   particle.E = sampleElementEnergy(seed, shared_data_, element_id);
   // Sample time
   particle.time = time_->sample(seed).first;
+
+  return particle;
 }
 
 extern "C" std::unique_ptr<FizzyCompiledSource>
